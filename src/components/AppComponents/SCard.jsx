1e8b89756d35850d75e9b2f5093354a574c15adb
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-12-29 18:08:57 +0600

    refactor: comment out unused state and effect hooks in SCard component

--- src/components/AppComponents/SCard.jsx
+++ src/components/AppComponents/SCard.jsx
@@ -1,38 +1,40 @@
-import { useContext, useEffect, useState } from "react";
-import { url } from "../../constants/constats";
-import { AuthContext } from "../../contexts/AuthProvider";
+// import { useContext, useEffect, useState } from "react";
+// import { url } from "../../constants/constats";
+// import { AuthContext } from "../../contexts/AuthProvider";
 import { Link } from "react-router";
 
 import { GiVote } from "react-icons/gi";
 import { BiLike } from "react-icons/bi";
 
 const SCard = ({ survey }) => {
-  const { user, access } = useContext(AuthContext);
-  const [likeCount, setLikeCount] = useState(0);
-  const [voteCount, setVoteCount] = useState(0);
+  // const { user, access } = useContext(AuthContext);
+  // const [likeCount, setLikeCount] = useState(0);
+  // const [voteCount, setVoteCount] = useState(0);
+  // console.log(user);
+  
 
 
-  useEffect(()=> {
-    fetch(`${url}/like/${survey._id}`, {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-        email: user?.email,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => setLikeCount(data.likeCount));
+  // useEffect(()=> {
+  //   fetch(`${url}/like/${survey._id}`, {
+  //     method: "GET",
+  //     headers: {
+  //       "content-type": "application/json",
+  //       email: user?.email,
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => setLikeCount(data.likeCount));
     
-      fetch(`${url}/totalvote/${survey._id}`, {
-        method: "GET",
-        headers: {
-          "content-type": "application/json",
-          email: user?.email,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => setVoteCount(data.totalVote));  
-  },[]);
+  //     fetch(`${url}/totalvote/${survey._id}`, {
+  //       method: "GET",
+  //       headers: {
+  //         "content-type": "application/json",
+  //         email: user?.email,
+  //       },
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => setVoteCount(data.totalVote));  
+  // },[]);
 
 
   return (
@@ -44,10 +46,14 @@ const SCard = ({ survey }) => {
          <div className="text-success text-xs border-0">{survey.category}</div>
          </div>
           <p className="py-5">{survey?.description?.length > 20 ? survey?.description?.slice(0, 20) + '...' : survey?.description}</p>
-          <div className="flex items-center justify-between">
-            <Link to={`/details/${survey._id}`} className="btn btn-success w-[55%]">Details</Link>
-            <div className="btn btn-success w-[20%]"><BiLike />: {survey.like}</div>
-            <div className="btn btn-success w-[20%]"><GiVote/>: {survey.vote}</div>
+          <div className="flex flex-col xl:flex-row items-center justify-between">
+            <div className="w-full flex xl:hidden justify-between mb-2">
+              <div className="btn btn-success w-[48%]"><BiLike />: {survey.like}</div>
+              <div className="btn btn-success w-[48%]"><GiVote/>: {survey.vote}</div>
+            </div>
+            <Link to={`/details/${survey._id}`} className="btn btn-success w-full xl:w-[55%]">Details</Link>
+            <div className="hidden xl:flex btn btn-success w-[20%]"><BiLike />: {survey.like}</div>
+            <div className="hidden xl:flex btn btn-success w-[20%]"><GiVote/>: {survey.vote}</div>
           </div>
         </div>
       </div>
