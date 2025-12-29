9c03f1e4c8fe046f4fba5406afd90bcbac888334
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-12-29 18:49:08 +0600

    fix: add loading state management to Featured and Latest Surveys components

--- src/components/AppComponents/featuredCompo/FeaturedSurveys.jsx
+++ src/components/AppComponents/featuredCompo/FeaturedSurveys.jsx
@@ -3,26 +3,39 @@ import SCard from "../SCard";
 import { url } from "../../../constants/constats";
 
 const FeaturedSurveys = () => {
+  const [loading, setLoading] = useState(true);
   const [surveys, setSurveys] = useState([]);
   useEffect(() => {
     fetch(`${url}/featured`)
       .then((res) => res.json())
-      .then((data) => setSurveys(data));
+      .then((data) => {
+        setSurveys(data)
+        setLoading(false);
+      });
   }, []);
 
   // console.log(surveys);
   return (
     <>
+
       <div className="bg-success/20 py-10">
         <h1 className="text-3xl font-bold text-center">
           Featured Survey&apos;s
         </h1>
-        <div className="container mx-auto  grid md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
-          {surveys?.map((survey) => {
-            return <SCard key={survey._id} survey={survey} />;
-          })}
-        </div>
+        {
+          loading && <div className="flex justify-center items-center min-h-75">
+            <span className="loading loading-bars loading-xl"></span>
+          </div>
+        }{
+          !loading &&
+          <div className="container mx-auto  grid md:grid-cols-2 lg:grid-cols-3 gap-5 p-5">
+            {surveys?.map((survey) => {
+              return <SCard key={survey._id} survey={survey} />;
+            })}
+          </div>
+        }
       </div>
+
     </>
   );
 };
